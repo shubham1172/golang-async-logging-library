d27509dfd001ad8b0da0afda21622a874b063c10
Shubham Sharma <shubhash@microsoft.com>
2021-02-14 13:05:16 +0530

    Graceful shutdown
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- alog.go
+++ alog.go
@@ -41,9 +41,21 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for {
-		msg := <-al.msgCh
-		go al.write(msg, nil)
+	var wg sync.WaitGroup
+
+	run := true
+	for run {
+		select {
+		// handle logging request
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			go al.write(msg, &wg)
+		// handle shutdown request
+		case _ = <-al.shutdownCh:
+			wg.Wait()
+			al.shutdown()
+			run = false
+		}
 	}
 }
 
@@ -55,6 +67,8 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	al.m.Lock()
 	defer al.m.Unlock()
 
@@ -65,6 +79,9 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	// notify shutdown complete
+	go func() { al.shutdownCompleteCh <- struct{}{} }()
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -82,6 +99,10 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	// initiate the shutdown
+	al.shutdownCh <- struct{}{}
+	// wait for the shutdown to complete
+	<-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
